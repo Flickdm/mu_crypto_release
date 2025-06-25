7198dfa162791c985ad4a1b80b3f9c68c3353f99
Doug Flick <dougflick@microsoft.com>
2025-06-25 14:44:04 -0700

    Moving SharedCrypto Application to MM_STANDALONE instance to overcome challenges in Mm Space

--- OpensslPkg/Library/SharedCryptLib/SharedCryptoBin.c
+++ OpensslPkg/Library/SharedCryptLib/SharedCryptoBin.c
@@ -7,15 +7,17 @@
 **/
 
 #include <Uefi.h>
-#include <Library/UefiApplicationEntryPoint.h>
+// #include <Library/StandaloneMmDriverEntryPoint.h> # Intentionally not included
+#include <Library/MmServicesTableLib.h>
 #include <Private/SharedCrtLibSupport.h>
 #include "Shared/SharedOpenssl.h"
+#include <Library/SharedCryptoDependencySupport.h>
 
 // #if defined(_MSC_VER)
 #define COMMON_EXPORT_API  __declspec(dllexport)
-// #else
-// #define COMMON_EXPORT_API
-// #endif
+
+
+SHARED_CRYPTO_MM_CONSTRUCTOR_PROTOCOL *ProtocolInstance = NULL;
 
 
 COMMON_EXPORT_API
@@ -45,27 +47,60 @@ Constructor (
 }
 
 /**
-  The user Entry Point for Application. The user code starts with this function
-  as the real entry point for the application.
+  MM Entry Point for the Shared Crypto MM Driver.
 
-  @param[in] ImageHandle        The firmware allocated handle for the EFI image.
-  @param[in] SystemTable        A pointer to the EFI System Table.
+  @param[in] ImageHandle      The firmware allocated handle for the EFI image.
+  @param[in] MmSystemTable    A pointer to the MM System Table.
 
-  @retval EFI_SUCCESS           The entry point is executed successfully.
-  @retval EFI_INVALID_PARAMETER SystemTable provided was not valid.
-  @retval other                 Some error occurs when executing this entry point.
-
+  @retval EFI_SUCCESS         The entry point executed successfully.
+  @retval EFI_OUT_OF_RESOURCES Failed to allocate memory.
+  @retval other               Error returned by protocol installation.
 **/
 EFI_STATUS
 EFIAPI
-UefiMain (
-  IN EFI_HANDLE        ImageHandle,
-  IN EFI_SYSTEM_TABLE  *SystemTable
+MmEntry (
+  IN EFI_HANDLE           ImageHandle,
+  IN EFI_MM_SYSTEM_TABLE  *MmSystemTable
   )
 {
-  SHARED_CRYPTO_PROTOCOL  Crypto;
+  EFI_STATUS Status;
+  EFI_HANDLE Handle = NULL;
 
-  CryptoInit (&Crypto);
+  if (MmSystemTable == NULL) {
+    DEBUG((DEBUG_ERROR, "SharedCryptoBin: MmSystemTable is NULL\n"));
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Status = MmSystemTable->MmAllocatePool (
+                    EfiRuntimeServicesData,
+                    sizeof (SHARED_CRYPTO_MM_CONSTRUCTOR_PROTOCOL),
+                    (VOID **)&ProtocolInstance
+                    );
+
+  if (EFI_ERROR(Status) || ProtocolInstance == NULL) {
+    DEBUG((DEBUG_ERROR, "SharedCryptoBin: Failed to allocate memory for constructor protocol: %r\n", Status));
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  ProtocolInstance->Signature = SHARED_CRYPTO_MM_CONSTRUCTOR_PROTOCOL_SIGNATURE;
+  ProtocolInstance->Version = 1;
+  ProtocolInstance->Constructor = Constructor;
+
+  Status = MmSystemTable->MmInstallProtocolInterface (
+                    &Handle,
+                    &gSharedCryptoPrivateProtocolGuid,
+                    EFI_NATIVE_INTERFACE,
+                    ProtocolInstance
+                    );
+
+  if (EFI_ERROR(Status)) {
+    DEBUG((DEBUG_ERROR, "SharedCryptoBin: Failed to install protocol: %r\n", Status));
+    MmSystemTable->MmFreePool(ProtocolInstance);
+    ProtocolInstance = NULL;
+    return Status;
+  }
+
+  DEBUG((DEBUG_INFO, "SharedCryptoBin: Protocol installed successfully\n"));
 
   return EFI_SUCCESS;
 }
