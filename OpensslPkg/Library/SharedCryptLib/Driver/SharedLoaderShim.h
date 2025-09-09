1bdb00b2433d22ad392d6779b8c08fa2070036fe
Doug Flick <dougflick@microsoft.com>
2025-09-09 16:03:17 -0700

    Allow system to LoadImage rather than driver

--- OpensslPkg/Library/SharedCryptLib/Driver/SharedLoaderShim.h
+++ OpensslPkg/Library/SharedCryptLib/Driver/SharedLoaderShim.h
@@ -3,6 +3,7 @@
 
 #include <Uefi.h>
 #include "Library/SharedCryptoDependencySupport.h"
+#include <Protocol/LoadedImage.h>
 
 //
 // These represent the dependencies that the driver needs to function
@@ -20,32 +21,5 @@ typedef struct _DRIVER_DEPENDENCIES {
 // Global variable to hold the driver dependencies
 //
 extern DRIVER_DEPENDENCIES  *gDriverDependencies;
-
-/**
- * @brief Phase-agnostic entry point for the driver.
- *
- * This function sets up the loader that will
- *  - Locate the DLLs that the driver depends on
- *  - Loads the DLL
- *  - Publishes the driver's protocol
- *
- * @param DllSectionData - The section data of the DLL
- * @param DllSectionDataSize - The size of the DLL section data
- *
- * @return EFI_STATUS
- * @retval EFI_SUCCESS The driver was loaded successfully
- * @retval EFI_OUT_OF_RESOURCES The driver was not loaded due to lack of resources
- * @retval EFI_INVALID_PARAMETER The driver was not loaded due to invalid parameters
- * @retval EFI_NOT_FOUND The driver was not loaded due to missing dependencies
- * @retval EFI_UNSUPPORTED The driver was not loaded due to unsupported features
- *
- */
-EFI_STATUS
-EFIAPI
-LoaderEntryPoint (
-  IN VOID          *DllSectionData,
-  IN UINTN         DllSectionDataSize,
-  OUT CONSTRUCTOR  *Constructor
-  );
 
 #endif // SHAREDLOADER_SHIM_H
